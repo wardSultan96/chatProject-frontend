bf427db859a477c9fbf44b475986c228d7d575b9
wardSultan96 <wardlinkedin96@gmail.com>
2025-07-03 18:08:59 +0300

    fix url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL ='http://localhost:3000';
 
 
-export const API_BASE_URL ='http://chatapp-backend-production-40dd.up.railway.app';
+export const API_BASE_URL ='https://chatproject-backend-production.up.railway.app';
 
 // chatapp-backend.railway.internal
